ab0e59b2684c0a75a8575b765c1bceab8246b88e
simplephi <cleverdarkness@gmail.com>
2018-10-05 02:24:20 +0800

    frontend

--- application/views/frontend/index.php
+++ application/views/frontend/index.php
@@ -305,10 +305,16 @@
 				<!-- START REVOLUTION SLIDER 5.0.7 fullwidth mode -->
 				<div id="rev_slider_34_1" class="rev_slider" data-version="5.0.7">
 					<ul>
+						<?php
+					 $no=1;
+					 foreach ($gambar as $key) {
+
+						// $id=$key->id_gambar;
+				 ?>
 						<!-- SLIDE  -->
 						<li data-index="rs-129"  >
 							<!-- MAIN IMAGE -->
-							<img src="<?php echo base_url()?>assets/assets/images/banner/slider1.jpg"  alt=""  class="rev-slidebg" >
+							<img src="<?php echo base_url()?>assets/foto/<?php echo $key->image_file;?>"  alt=""  class="rev-slidebg" >
 							<!-- LAYERS -->
 							<!-- LAYER NR. 2 -->
 							<div class="tp-caption Newspaper-Title tp-resizeme "
@@ -331,82 +337,17 @@
 							data-responsive_offset="on" >
 								<div class="banner-text">
 									<span class="sub-text">we are here to help you</span>
-									<h2>The best cleaning company in the country</h2>
+									<h2><?php echo $key->image_name;?></h2>
 									<p>
-										Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever.
+										<?php echo $key->image_desc;?>
 									</p>
 									<a class="btn-text" href="#"> Read More</a>
 								</div>
 							</div>
 						</li>
-						<!-- SLIDE  -->
-						<li data-index="rs-130" data-title="" data-description="">
-							<!-- MAIN IMAGE -->
-							<img src="<?php echo base_url()?>assets/assets/images/banner/slider2.jpg"  alt=""   class="rev-slidebg">
-							<!-- LAYERS -->
-							<!-- LAYER NR. 2 -->
-							<div class="tp-caption Newspaper-Title   tp-resizeme "
-							id="slide-130-layer-1"
-							data-x="['left','left','left','left']" data-hoffset="['100','50','50','30']"
-							data-y="['top','top','top','center']" data-voffset="['230','135','50','0']"
-							data-fontsize="['50','50','50','30']"
-							data-lineheight="['55','55','55','35']"
-							data-width="['700','700','700','420']"
-							data-height="none"
-							data-whitespace="normal"
-							data-transform_idle="o:1;"
-							data-transform_in="y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power3.easeInOut;"
-							data-transform_out="auto:auto;s:1000;e:Power3.easeInOut;"
-							data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
-							data-mask_out="x:0;y:0;s:inherit;e:inherit;"
-							data-start="1000"
-							data-splitin="none"
-							data-splitout="none"
-							data-responsive_offset="on">
-								<div class="banner-text">
-									<span class="sub-text">we are here to help you</span>
-									<h2>Professional Cleaning Services Provider</h2>
-									<p>
-										Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever.
-									</p>
-									<a class="btn-text" href="#"> Read More</a>
-								</div>
-							</div>
-						</li>
-						<!-- SLIDE  -->
-						<li data-index="rs-131">
-							<!-- MAIN IMAGE -->
-							<img src="<?php echo base_url()?>assets/assets/images/banner/slider3.jpg"  alt=""   class="rev-slidebg " >
-							<!-- LAYERS -->
-							<!-- LAYER NR. 2 -->
-							<div class="tp-caption Newspaper-Title   tp-resizeme "
-							id="slide-131-layer-1"
-							data-x="['left','left','left','left']" data-hoffset="['100','50','50','30']"
-							data-y="['top','top','top','center']" data-voffset="['230','135','50','0']"
-							data-fontsize="['50','50','50','30']"
-							data-lineheight="['55','55','55','35']"
-							data-width="['700','700','700','420']"
-							data-height="none"
-							data-whitespace="normal"
-							data-transform_idle="o:1;"
-							data-transform_in="y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power3.easeInOut;"
-							data-transform_out="auto:auto;s:1000;e:Power3.easeInOut;"
-							data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
-							data-mask_out="x:0;y:0;s:inherit;e:inherit;"
-							data-start="1000"
-							data-splitin="none"
-							data-splitout="none"
-							data-responsive_offset="on">
-								<div class="banner-text">
-									<span class="sub-text">we are here to help you</span>
-									<h2>We Are Cleaning Manager Always at Your Service.</h2>
-									<p>
-										Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever.
-									</p>
-									<a class="btn-text" href="#"> Read More</a>
-								</div>
-							</div>
-						</li>
+						<?php
+					}
+				?>
 						<!-- SLIDE  -->
 					</ul>
 					<div class="tp-bannertimer tp-bottom"></div>
@@ -421,7 +362,7 @@
 				<div class="row pb-30">
 					<div class="col-md-4 d-flex align-items-center">
 						<div class="section_tit">
-							<h2>About Us</h2>
+							<h2>Desa Laywo</h2>
 							<span class="b-line l-left sm-l-center"></span>
 						</div>
 					</div>
@@ -436,52 +377,6 @@
 					</div>
 				</div>
 
-				<div class="row mt-30 mt-xs-0">
-					<div class="col-lg-3 col-md-6">
-						<div class="clean_top mb-xs-30 feature-box">
-							<div class="content">
-								<img src="assets/images/cleaning-lady.svg" alt="" height="50" width="50" />
-								<h3>Expert</h3>
-								<p>
-									Lorem Ipsum is simply text the printing and typesetting standard industry.
-								</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-6">
-						<div class="clean_top mb-xs-30 feature-box">
-							<div class="content">
-								<img src="assets/images/cleaner.svg" alt="" height="50" width="50" />
-								<h3>Secure Services</h3>
-								<p>
-									Lorem Ipsum is simply text the printing and typesetting standard industry.
-								</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-6 mt-sm-30">
-						<div class="clean_top mb-xs-30 feature-box">
-							<div class="content">
-								<img src="assets/images/clean.svg" alt="" height="50" width="50" />
-								<h3>Low Costing</h3>
-								<p>
-									Lorem Ipsum is simply text the printing and typesetting standard industry.
-								</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-6 mt-sm-30">
-						<div class="clean_top mb-xs-30 feature-box">
-							<div class="content">
-								<img src="assets/images/clean-1.svg" alt="" height="50" width="50" />
-								<h3>On Time</h3>
-								<p>
-									Lorem Ipsum is simply text the printing and typesetting standard industry.
-								</p>
-							</div>
-						</div>
-					</div>
-				</div>
 
 			</div>
 		</section>
